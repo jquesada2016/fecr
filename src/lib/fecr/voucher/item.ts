6ac5a98f889a5e119c3428cc4fb4f93926f7631c
Jose Quesada <jquesada2016@fau.edu>
2020-11-19 10:57:50 -0500

    completed character and array length validation agains XSD documentation

--- src/lib/fecr/voucher/item.ts
+++ src/lib/fecr/voucher/item.ts
@@ -3,38 +3,64 @@ import {
   generateItem,
   discount,
   unidadMedida,
-  commercialCodeType,
   tax
 } from "../../../types";
+import { commercialCode } from "../../../types/types";
 
 export class Item {
   constructor(item: item) {
     this.number = item.number;
     if (item.hsCode) {
-      this.hsCode = item.hsCode;
+      if (item.hsCode.length === 13) this.hsCode = item.hsCode;
+      else throw new Error("hsCode must be 13 characters in length");
     }
     this.hsCode = item.hsCode;
 
     if (item.commercialCode) {
-      this.typeCommercialCode = item.commercialCode.Tipo;
-      this.commercialCode = item.commercialCode.Codigo;
+      if (item.commercialCode.length <= 5)
+        this.commercialCode = item.commercialCode;
+      else throw new Error("commercialCode must be <= 5 items");
+      item.commercialCode.map((item, i) => {
+        if (item.Codigo!.length > 20)
+          throw new Error(
+            `commercialCode Codigo at index ${i} must be <= 20 characters in length`
+          );
+      });
     }
     this.quantity = item.quantity;
     this.units = item.units;
     if (item.commercialUnits) {
-      this.commercialUnits = item.commercialUnits;
+      if (item.commercialUnits.length <= 20)
+        this.commercialUnits = item.commercialUnits;
+      else
+        throw new Error("commercialUnits must be <= 20 characters in length");
     }
-    this.description = item.detail;
+    if (item.detail.length <= 200) this.detail = item.detail;
+    else throw new Error("detail must be <= 200 characters in length");
     this.unitPrice = item.unitPrice;
     this.total = item.total;
     if (item.discount) {
-      this.discount = item.discount;
+      if (item.discount.length <= 5) {
+        item.discount.map((item, i) => {
+          if (item.NaturalezaDescuento.length > 80)
+            throw new Error(
+              `discount NaturalezaDescuento on index ${i} must be <= 80 characters in length`
+            );
+        });
+        this.discount = item.discount;
+      } else throw new Error("discount must be <= 5 items in length");
     }
     this.subtotal = item.subtotal;
     if (item.taxBase) {
       this.taxBase = item.taxBase;
     }
     if (item.taxes) {
+      item.taxes.map((item, i) => {
+        if (item.Exoneracion.NombreInstitucion.length > 160)
+          throw new Error(
+            `NombreInstitucional on index ${i} must be <= 160 characters in length`
+          );
+      });
       this.taxes = item.taxes;
     }
     this.taxNet = item.taxNet ? item.taxNet : 0;
@@ -43,12 +69,11 @@ export class Item {
 
   number: number;
   hsCode: string;
-  typeCommercialCode?: commercialCodeType;
-  commercialCode?: string;
+  commercialCode?: commercialCode;
   quantity: number;
   units: unidadMedida;
   commercialUnits?: string;
-  description: string;
+  detail: string;
   unitPrice: number;
   total: number;
   discount?: discount;
@@ -61,31 +86,22 @@ export class Item {
   generate() {
     const obj = {} as generateItem;
     obj.NumeroLinea = this.number;
-    if (this.hsCode) {
-      obj.Codigo = this.hsCode;
-    }
-    if (this.commercialCode && this.typeCommercialCode) {
-      obj.CodigoComercial = {
-        Tipo: this.typeCommercialCode,
-        Codigo: this.commercialCode
-      };
+    obj.Codigo = this.hsCode;
+    if (this.commercialCode) {
+      obj.CodigoComercial = [...this.commercialCode];
     }
     obj.Cantidad = this.quantity;
     obj.UnidadMedida = this.units;
-    obj.Detalle = this.description;
+    obj.Detalle = this.detail;
     obj.PrecioUnitario = this.unitPrice;
     obj.MontoTotal = this.total;
     if (this.discount) {
-      obj.Descuento = this.discount;
+      obj.Descuento = [...this.discount];
     }
     obj.SubTotal = this.subtotal;
     obj.BaseImponible = this.taxBase;
     if (this.taxes) {
-      const iva: any = [];
-      this.taxes.forEach((tax: any) => {
-        iva.push(tax.generate());
-      });
-      obj.Impuesto = iva;
+      obj.Impuesto = [...this.taxes];
     }
     obj.ImpuestoNeto = this.taxNet!;
     obj.MontoTotalLinea = this.netTotal;
